4885621b01391d66d454b3b9770134cab5e36e83
jackil182 <jackil182@gmail.com>
2019-04-12 16:20:48 +0300

    fixed bug when providing location not in english language

--- utils/geocode.js
+++ utils/geocode.js
@@ -6,14 +6,14 @@ const getGeoData = (address, cb) => {
     'pk.eyJ1IjoiamFja2lsMTgyIiwiYSI6ImNqdTRmaHc1NTB1a2I0NGxsNm13OW42d2EifQ.5TfjMcxRnTAMrv_NZ7Cw2w';
   const mapUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${mapKey}&limit=1`;
 
-  request({ url: mapUrl, json: true }, (err, {body}) => {
+  request({ url: mapUrl, json: true }, (err, data) => {
     if (err) {
-      cb('unable to connect to geo-service');
-    } else if (body.features.length === 0) {
+      cb('unable to connect to geo-service, make sure you provide the location in English');
+    } else if (typeof data === 'undefined' || data.body.features.length === 0) {
       cb('address is incorrect');
     } else {
-      const [long, lat] = body.features[0].center;
-      const location = body.features[0].place_name;
+      const [long, lat] = data.body.features[0].center;
+      const location = data.body.features[0].place_name;
       cb(null, {long, lat, location});
     }
   });
